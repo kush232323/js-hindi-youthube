54b0ee0d60670def495b8800c0ecc88d6b1a7932
kush232323 <142990080+kush232323@users.noreply.github.com>
2023-08-24 18:45:05 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "333abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+ 
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 30
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
