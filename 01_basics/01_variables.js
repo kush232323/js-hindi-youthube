6d421c517e45d1b8b91ba4b53e11a53a8682d781
kush232323 <142990080+kush232323@users.noreply.github.com>
2023-08-24 17:34:26 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "hitesh@goole.com"
+var accountPassword = "12345"
+accountCity = "patna"
+
+// accountId = 2// not allowed
+
+
+accountEmail ="hc@ha.com"
+accountpassword = "21212121"
+accountCity = "jaipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity ])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version 
+
+// aleart (3 + 3)//we are using node.js, not browser
+
+console.log( 3 + 3); console.log("hitesh")
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint => big number
+// string = ""
+//boolean = ture /false
+//null = standa alone value
+// undifined =
+// symbol
