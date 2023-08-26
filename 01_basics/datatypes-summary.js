f37cc8ea1306531f8059d66ad51f1ee3fc5e0cc7
kush232323 <142990080+kush232323@users.noreply.github.com>
2023-08-26 18:17:45 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,20 @@
+ const score = 100
+ const scoreValue = 100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null
+ let userEMAIL;
+
+
+ const id  = Symbol('123')
+ const anotherId = Symbol('123')
+
+//  console.log(id === anotherId);
+//  const bigNumber = 1323213312123233232312312312312321321321313213131313n
+ const myfunction = function() {
+    console.log("hello world");
+
+    
+    
+ }
+ console.log(typeof myfunction);
