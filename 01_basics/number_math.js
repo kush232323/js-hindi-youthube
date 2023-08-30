1dfb3516fb8aa8d98738df8fbb9fe4d385cebb42
kush232323 <142990080+kush232323@users.noreply.github.com>
2023-08-30 10:44:40 +0000

    number and math

--- 01_basics/number_math.js
+++ 01_basics/number_math.js
@@ -1,2 +1,22 @@
 const score = 400
-console.log(score);
+// console.log(score);
+
+ const balance = new Number(100)
+//  console.log(balance)
+
+//  console.log(balance.toString().length);
+//  console.log(balance.toFixed(2));
+
+ const otherNumber = 1123.8966
+//  console.log(otherNumber.toPrecision(3));
+ const hundreds = 1000000000
+//  console.log(hundreds.toLocaleString('en-IN'));
+
+//  ++++++++++++++++++++++++++++++++++++++++ math ++++++++++++++++++++++++++++
+
+console.log(Math); 
+
+
+
+
+
