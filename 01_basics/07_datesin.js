7e3f05c11e3b5f735d47b7ea29197b89f5aee2cd
kush232323 <142990080+kush232323@users.noreply.github.com>
2023-09-06 12:23:10 +0000

    datesinjs

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,26 @@
+// dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 23 , 5, 3)
+// console.log(myCreatedDate.toLocaleString());
+
+
+console.log("nishanr");
+let myCreatedDate = new Date("01-12-23");
+// console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+
+
+newDATE.ToLocaleString('defult', {
+    weekday: "long",
+})
